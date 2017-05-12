cfa072a831b04dbd283d4e1113bf3d653a5d1285
Matt J Raibert <mattraibert@positiondev.com>
2017-05-12 19:06:08 -0400

    Look, a database!

--- src/main/java/com/position/cart/cart/HelloController.java
+++ src/main/java/com/position/cart/cart/HelloController.java
@@ -1,14 +1,17 @@
 package com.position.cart.cart;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
 
+  @Autowired
+  CustomerRepository customerRepository;
+
   @RequestMapping("/")
   public String index() {
-    return "Greetings from Spring Boot!";
+    return String.valueOf(customerRepository.findAll());
   }
-
 }

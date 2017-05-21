b85a9737282e97393e411e7ed2b4cda518c5b290
Matt J Raibert <mattraibert@positiondev.com>
2017-05-21 09:20:03 -0400

    add a product to the cart from the product(s) page

--- posto.md
+++ posto.md
@@ -1,15 +1,14 @@
 todo
 ----
 
-1. add a product to the cart from the product(s) page
-2. create a new product
-3. show cart total on cart page
-4. process cart total through stripe
-5. create new customers
-6. customer can log in
-7. customer can only see their own carts
-8. logged in customer can see their receipts
-9. send a receipt email after purchase
-10. Try view negotiation with: https://spring.io/blog/2013/06/03/content-negotiation-using-views/
-11. Deploy on heroku or aws
-12. Use external tomcat in production
+1. create a new product
+2. show cart total on cart page
+3. process cart total through stripe
+4. create new customers
+5. customer can log in
+6. customer can only see their own carts
+7. logged in customer can see their receipts
+8. send a receipt email after purchase
+9. Try view negotiation with: https://spring.io/blog/2013/06/03/content-negotiation-using-views/
+10. Deploy on heroku or aws
+11. Use external tomcat in production
--- src/main/java/com/positiondev/cart/product/ProductController.java
+++ src/main/java/com/positiondev/cart/product/ProductController.java
@@ -1,5 +1,8 @@
 package com.positiondev.cart.product;
 
+import com.positiondev.cart.cart.Cart;
+import com.positiondev.cart.cart.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+  @Autowired CartRepository cartRepository;
+
   @GetMapping("/{id}.json")
   public Product productjson(@PathVariable("id") Product product) {
     return product;
@@ -16,6 +21,9 @@ public class ProductController {
 
   @GetMapping("/{id}")
   public ModelAndView product(@PathVariable("id") Product product) {
-    return new ModelAndView("product", "product", product);
+    Cart cart = cartRepository.findAll().iterator().next();
+    ModelAndView mav = new ModelAndView("product", "product", product);
+    mav.addObject("cart", cart);
+    return mav;
   }
 }
